62bddb23b3f94d66764a9eab520d4dbafe0ba5b1
brunotavaresz <brunotavaresmeixedo@ua.pt>
2025-03-29 22:52:52 +0000

    done

---
+++ lab6/lab6_1/cars/src/test/java/com/example/cars/TestCarsApplication.java
@@ -0,0 +1,11 @@
+package com.example.cars;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(CarsApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
